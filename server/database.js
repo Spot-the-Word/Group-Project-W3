37d5abb96ed00fb4fe662f2644da6b3fbd26a290
Puspa Widya <puspaparamitha24@gmail>
2021-03-18 21:34:13 +0700

    Basic database for quiz

--- server/database.js
+++ server/database.js
@@ -1,8 +1,44 @@
 const words = [
   {
-    word: '',
-    clue: ''
+    word: 'Error',
+    clue: 'Teman saat mengoding'
   },
+  {
+    word: 'Anjing',
+    clue: 'Ferguso adalah seekor'
+  },
+  {
+    word: 'Rubah',
+    clue: 'Maskot Hactiv8'
+  },
+  {
+    word: 'Numerouno',
+    clue: 'Kopi buatan mu ...'
+  },
+  {
+    word: 'Pinokio',
+    clue: 'Hidung saya bisa memanjang'
+  },
+  {
+    word: 'Cinderella',
+    clue: 'Saya punya sepatu dari kaca'
+  },
+  {
+    word: 'Tokyo',
+    clue: 'Disneyland Jepang berada di kota'
+  },
+  {
+    word: 'Singa',
+    clue: 'Icon singapura'
+  },
+  {
+    word: 'Ikan',
+    clue: 'Nemo adalah seekor'
+  },
+  {
+    word: '9',
+    clue: 'Tebak angka (1-10)'
+  }
 ]
 
 module.exports = words
